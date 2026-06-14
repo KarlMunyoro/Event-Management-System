e71f3c69e0a673f701a444b42e03902ad03e7476
Karl Munyoro <kxrlmunyoro@gmail.com>
2026-06-14 23:55:08 +0300

    Completed Stage 1

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import './config/db.js'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Backend isnt updating!' })
+})
+
+const PORT = 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
